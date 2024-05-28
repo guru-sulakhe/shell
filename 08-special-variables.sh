44388d52038d17d91a7da039b0a33bf092949ad6
guru-sulakhe <sulakheguruprasad@gmail.com>
2024-05-28 20:24:24 +0530

    username and password

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -5,6 +5,7 @@ echo "Number of variables passed : $#"
 echo "Script  Name : $0"
 echo "Current working directory : $PWD"
 echo "Home directory of the current user : $HOME"
+echo "Which user is running the current script: $USER" 
 echo "HostName : $HOSTNAME"
 echo "Proceed ID of the current shell script : $$"
 sleep 30 & # which will run in the background with 30 seconcds in order to know about the process instance id of this command ,type as ps -ef
