438aa9f453a59677648c5a848a10c7babe09f417
guru-sulakhe <sulakheguruprasad@gmail.com>
2024-05-28 19:56:07 +0530

    username and password

---
+++ 07-sum-values.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+NO1=$1 # Here $1 represents first arguement which is passsed during running of the script 
+NO2=$2 # Here $2 represents second arguement which is passsed during running of the script 
+
+SUM=$(($NO1+$NO2))
+echo "Sum of $NO1 and $NO2 is : $SUM"
