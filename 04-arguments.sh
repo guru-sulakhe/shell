a68a1c94cbaef9404f6514c96867eca8ef2fc299
guru-sulakhe <sulakheguruprasad@gmail.com>
2024-05-28 13:33:07 +0530

    passing arguments

---
+++ 04-arguments.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#declaration of variables
+PERSON1=$1 #no sparce shoud not be added between = and value
+PERSON2=$2
+
+#refering  of variable
+echo "$PERSON1 :: Hello Suresh,how are you?"
+echo "$PERSON2 :: Hi $PERSON1, I am fine, How are you?"
+echo "$PERSON1 :: I am fine too, how is your work?"
+echo "$PERSON2 :: not bad, I am thinking to upgrade to devops"
