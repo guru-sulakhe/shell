3bef5375a7d66814d78019e8c6eeaf64765cc6f8
guru-sulakhe <sulakheguruprasad@gmail.com>
2024-05-31 16:45:37 +0530

    functions

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
 USERID=$(id -u) #script execute and store the output in USERID
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPTNAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+R="\e[31m" #for red color
+G="\e[32m" #for green color
+Y="\e[33m" #for yellow color
+N="\e[0m" #for normal color
 
-if [ USERID -ne 0 ]
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2.. FAILED"
+    else
+        echo "$2.. SUCCESSFUL" 
+    fi
+}
+
+if [ $USERID -ne 0 ]
 then
     echo "Only super user have access to install the packages"
     exit 1
@@ -13,4 +29,11 @@ fi
 for i in $@ # $@ represents all variables, it is important to give packages name whenever running the script
 do
     echo "packages to install : $i"  # Here $i represents variable names given by user to install,for each iteration it will take one package to install
+    dnf list installed $i &>>$LOGFILE
+    if [ $? -eq 0 ]
+    then
+        echo "already installed $i package,$Y skipping.. $N"
+    else
+        echo "$i is not installed, Need to install"
+    fi
 done
