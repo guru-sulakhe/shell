6ed34f4c78d6195f05f6c0190bf4bd5de4ee91af
guru-sulakhe <sulakheguruprasad@gmail.com>
2024-05-31 12:35:15 +0530

    installing mysql

--- 10-install-mysql.sh
+++ 10-install-mysql.sh
@@ -5,10 +5,30 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "can't able to install MYSQL, only root user can have permission to install it"
-    exit 1
+    exit 1 # manually exit if error occurs
 
 else
     echo "you are a super user"
 
 fi
 dnf install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of MYSQL.. is failed"
+    exit 1
+else
+    echo "Installation of MYSQL.. is successful"
+fi
+
+dnf install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of GIT.. is failed"
+    exit 1
+else
+    echo "Installation of GIT.. is successful"
+fi
+
+echo "is script is proceeding"
