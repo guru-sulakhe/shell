eb5ded0a1df1255541d35434d4e3745e868f2c56
guru-sulakhe <sulakheguruprasad@gmail.com>
2024-05-31 12:12:56 +0530

    installing mysql

---
+++ 10-install-mysql.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0]
+then
+    echo "can't able to install MYSQL, only root user can have permission to install it"
+    exit 1
+
+else
+    echo "you are a super user"
+
+fi
+dnf install mysql -y
