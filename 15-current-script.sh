c816871eb77fa933cacd3e7ed046de2a523c8030
guru-sulakhe <sulakheguruprasad@gmail.com>
2024-06-07 11:39:35 +0530

    shell

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -5,6 +5,11 @@ COURSE="Devops from current script"
 echo "Before calling other script, course: $COURSE"
 echo "Process ID of the current script: $$"
 
-./16-other-script.sh
+#./16-other-script.sh it is a first way of calling other script.
+source ./16-other-script.sh #it is second way of calling other script.
 
 echo "After calling other script,course: $COURSE"
+
+#NOTE:
+#while calling other script using first way:variable value after calling other script will not change,PID of two scripts are different
+#while calling other scrpt using second way:variable value after calling other script will change,PID of two scripts are same
