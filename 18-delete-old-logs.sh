fb17aadf4c7ddc930d4f7772752fe780d2684207
guru-sulakhe <sulakheguruprasad@gmail.com>
2024-06-11 14:11:43 +0530

    shell

--- 18-delete-old-logs.sh
+++ 18-delete-old-logs.sh
@@ -16,6 +16,6 @@ FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
 
 while IFS= read -r line
 do
-    echo "Deleting file: $line
+    echo "Deleting file: $line"
     rm -rf $line
-done <<< $FILES
+done
